422ce62d4cf236893145a3fa57ed5b38ed7911da
IvanIaffar <ivan.iaffar@gmail.com>
2026-05-26 16:15:44 -0300

    TP C# punto b

--- ExMetodosVirtuales/Form1.cs
+++ ExMetodosVirtuales/Form1.cs
@@ -25,11 +25,11 @@ namespace Figuras
         private void button1_Click(object sender, EventArgs e)
         {
             Graphics gr = pictureBox1.CreateGraphics();
-            // Pen pen = new Pen(Color.Black);
-            Pen pen = new Pen(Color.Red);
+            Color[] colores = new Color[] { Color.Red, Color.Blue, Color.Green };
             for (int i = 0; i < figuras.Length; i++)
             {
-                figuras[i].Dibujar(pen,gr,i * 100, 50);
+                Pen pen = new Pen(colores[i]);
+                figuras[i].Dibujar(pen, gr, i * 100, 50);
             }
 
         }
